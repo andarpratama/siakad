1caa6d56d5dd20104d83d8cf30c61955ec705cb9
Andar Pratama <master.amarta@gmail.com>
2021-04-25 20:22:47 +0700

    make migration classes and make controller + view Home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ url('bootstrap4/css/bootstrap.min.css') }}">
+    <title>Home - Sistem Informasi Kampus</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">Sistem Informasi Kampus</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse ml-4" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link mr-4  active" href="#">Home</a>
+                    <a class="nav-link mr-4 " href="#">Data Mahasiswa</a>
+                    <a class="nav-link mr-4 " href="#">Data Dosen</a>
+                    <a class="nav-link mr-4 " href="#">Data Kelas</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+
+    <div class="container mt-4">
+        <div class="jumbotron bg-white shadow-sm">
+            <h1 class="display-4">Sistem Informasi Kampus</h1>
+            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <hr class="my-4">
+            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <a class="btn btn-outline-dark btn-lg" href="#" role="button">Daftar Mahasiswa</a>
+        </div>
+    </div>
+
+
+</body>
+</html>
